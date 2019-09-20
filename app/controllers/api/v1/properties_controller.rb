11c3b060ce5f7fd0f04d1725d157438c661c5025
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-20 10:48:21 -0700

    index for properties set up

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::PropertiesController < ApplicationController
+    def index 
+        properties = Property.all
+        render json: PropertySerializer.new(properties).to_serialized_json
+    end
+
+    # def create
+    #     property = Property.new(property_params)
+    #     if property.save!
+    #         render json: property_params
+    #     else
+    #         render :new 
+    #     end
+    # end
+
+    # private 
+
+    # def property_params
+    #     params.permit(:id, :high_property, :display_name)
+    # end
 end
--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,3 +1,12 @@
 class PropertySerializer < ActiveModel::Serializer
-  attributes :id
+  def initialize(property_object)
+    @property = property_object
+  end
+
+  def to_serialized_json
+      options = {
+        except: [:updated_at, :created_at]
+      }
+      @property.to_json(options)
+  end
 end
