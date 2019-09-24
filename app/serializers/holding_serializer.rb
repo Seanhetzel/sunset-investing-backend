78ae26ff9dff77225b22acb451e7a6de690420cd
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-24 10:02:59 -0700

    added amount to holdings and set up holdings controller

--- app/serializers/holding_serializer.rb
+++ app/serializers/holding_serializer.rb
@@ -1,3 +1,12 @@
 class HoldingSerializer < ActiveModel::Serializer
-  attributes :id
+    def initialize(holding_object)
+        @holding = holding_object
+    end
+
+    def to_serialized_json
+        options = {
+            except: [:updated_at, :created_at]
+        }
+        @holding.to_json(options)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_172840) do
+ActiveRecord::Schema.define(version: 2019_09_24_162651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_20_172840) do
     t.integer "property_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "amount"
   end
 
   create_table "investors", force: :cascade do |t|
