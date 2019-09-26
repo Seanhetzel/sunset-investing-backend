b10709de2f433cf1bd811caca539aa0cd11dca4c
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-26 15:38:10 -0700

    adding auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_162651) do
+ActiveRecord::Schema.define(version: 2019_09_26_223644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_162651) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "properties", force: :cascade do |t|
